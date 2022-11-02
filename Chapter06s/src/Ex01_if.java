76c4c09082d4ade7495f1a166dc605f45f62be49
hyesun22 <110083948+hyesun22@users.noreply.github.com>
2022-11-02 23:31:34 +0900

    commit

---
+++ Chapter06s/src/Ex01_if.java
@@ -0,0 +1,29 @@
+
+public class Ex01_if
+{
+
+	public static void main(String[] args)
+	{
+		int num = 100;
+		
+		if (num > 50)
+		{
+			System.out.println("num 변수의 값이 50보다 큽니다.");
+		}
+		
+		
+		//한 줄만 실행하면 중괄호를 생략 가능
+		if (num > 50)
+			System.out.println("num 변수의 값이 50보다 큽니다.");
+		
+		//같은 줄에 작성 가능. 실행 문장의 끝은 세미콜론 ;으로 구분
+		if (num > 50) System.out.println("num 변수의 값이 50보다 큽니다.");
+		
+		//의도치 않은 결과
+		if (num < 50)
+			System.out.println(num); // num변수의 값이 50보다 작다는 명제는 거짓이므로 이건 실행되지 않는다.
+		    System.out.println("num 변수의 값이 50보다 작습니다."); //그냥 출력문
+
+	}
+
+}
