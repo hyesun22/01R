04c447d2674904dde607fe0979313266fa95abed
hyesun22 <98395122+solra59@users.noreply.github.com>
2022-12-12 21:22:51 +0900

    commit

--- 이재환의 자바 프로그래밍 입문/실습/Chapter14s/src/Ex02_RefCompare.java
+++ 이재환의 자바 프로그래밍 입문/실습/Chapter14s/src/Ex02_RefCompare.java
@@ -9,12 +9,12 @@ public class Ex02_RefCompare {
 		if(str1 == str2)
 			System.out.println("str1과 str2는 동일 객체 참조");
 		else
-			System.out.println("str1과 str2는 다른 객체 참조");
+			System.out.println("str1과 str2는 다른 객체 참조"); //서로 다른 객체의 id값을 가지고 있으므로 이 줄이 출력됨
 		
 		if(str3 == str4)
-			System.out.println("str과 str4는 동일 객체 참조");
+			System.out.println("str1과 str4는 동일 객체 참조");
 		else
-			System.out.println("str3과 str4는 다른 객체 참조");
+			System.out.println("str3과 str4는 다른 객체 참조"); //같은 객체의 참조 id가 들어있다.
 	}
 
-}
+} //*결론: 비교 연산자로는 문자열의 내용을 비교할 수 없다.
